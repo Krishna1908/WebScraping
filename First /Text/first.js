5aa5d986c3c8ab1a0f6693e8dd179bd4c4f5cbe6
Krishna1908 <113039173+Krishna1908@users.noreply.github.com>
2023-09-29 15:22:01 +0530

    Add files via upload

---
+++ First /Text/first.js
@@ -0,0 +1,18 @@
+// Sample JavaScript file
+
+// Define some variables
+const name = "John Doe";
+const age = 30;
+const city = "New York";
+
+// Display a message using the variables
+console.log(`My name is ${name}.`);
+console.log(`I am ${age} years old.`);
+console.log(`I live in ${city}.`);
+
+// Create an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// Calculate and display the sum of the numbers
+const sum = numbers.reduce((acc, current) => acc + current, 0);
+console.log(`The sum of the numbers is ${sum}.`);
